16c315d461900111e96097143b04a371272232e2
Antonio081014 <antonio081014@antonio081014.com>
2015-08-23 00:59:07 -0700

    Version 7
    Added Grab-Move-Drop function to each dot view. User could move 2 or
    more dot views simultaneously since the system supports rest touching
    function.
    While the pan gesture to scroll the scrollview is disabled when moving
    the dot view.

--- TouchDemo/ViewController.m
+++ TouchDemo/ViewController.m
@@ -60,8 +60,59 @@
 
 - (void)handleLongPress:(UILongPressGestureRecognizer *)gesture
 {
+    UIView *dot = gesture.view;
     
+    switch (gesture.state) {
+        case UIGestureRecognizerStateBegan:
+            [self grabDot:dot withGesture:gesture];
+            break;
+        case UIGestureRecognizerStateChanged:
+            [self moveDot:dot withGesture:gesture];
+            break;
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+            [self dropDot:dot withGesture:gesture];
+            break;
+        default:
+            break;
+    }
 }
 
+- (void)grabDot:(UIView *)dot withGesture:(UIGestureRecognizer *)gesture
+{
+    dot.center = [self.view convertPoint:dot.center fromView:dot.superview];
+    [self.view addSubview:dot];
+    [UIView animateWithDuration:.2f animations:^{
+        dot.transform = CGAffineTransformMakeScale(1.2f, 1.2f);
+        dot.alpha = .8f;
+        [self moveDot:dot withGesture:gesture];
+    }];
+    
+    [DotView arrangeDotsNeatlyInViewWithNiftyAnimation:self.drawerView.contentView];
+}
+
+- (void)moveDot:(UIView *)dot withGesture:(UIGestureRecognizer *)gesture
+{
+    dot.center = [gesture locationInView:self.view];
+}
+
+- (void)dropDot:(UIView *)dot withGesture:(UIGestureRecognizer *)gesture
+{
+    [UIView animateWithDuration:.2f animations:^{
+        dot.transform = CGAffineTransformIdentity;
+        dot.alpha = 1.f;
+    }];
+    
+    CGPoint location = [gesture locationInView:self.drawerView];
+    if (CGRectContainsPoint(self.drawerView.bounds, location)) {
+        [self.drawerView.contentView addSubview:dot];
+    } else {
+        [self.canvasView addSubview:dot];
+    }
+    
+    dot.center = [self.view convertPoint:dot.center fromView:dot.superview];
+
+    [DotView arrangeDotsNeatlyInViewWithNiftyAnimation:self.drawerView.contentView];
+}
 
 @end
